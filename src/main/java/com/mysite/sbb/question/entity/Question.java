bd63ee98bd9ad4caaf781c8dcb5f60347b32e85e
Hjun1004 <hjun1004@naver.com>
2023-03-20 15:43:27 +0900

    OneToMany 주석 추가

--- src/main/java/com/mysite/sbb/question/entity/Question.java
+++ src/main/java/com/mysite/sbb/question/entity/Question.java
@@ -34,6 +34,12 @@ public class Question {
     @CreatedDate
     private LocalDateTime create_date;
 
+    // OneToMany는 자바세상에서의 편의를 위해서 필드를 생성했다.
+    // 이 녀석은 실제 DB칼럼이 생성되지 않는다.
+    // DB는 배열이나 리스트를 저장할 수 없다.
+        // 칼럼에 저장할 수 없다.
+    // 만들어도 되고 안 만들어도 됩니다.
+    // 다만 만들면 자바에서 해당 객체(질문객체)에서 관련된 답변들을 찾을 때 편하다.
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) //@OneToMany 애너테이션에 사용된 mappedBy는 참조 엔티티의 속성명을 의미
     // 질문이 삭제되면 달렸던 답변들도 다 삭제되게 함
     private List<Answer> answerList;
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,25 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.question.entity.Question;
+import com.mysite.sbb.question.questionRepository.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		this.questionRepository.save(q1);
+
+
 	}
 
 }
